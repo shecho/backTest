343eb94f68a51d796664ed1cfe0aa53bd225aa01
svalencia <svalencia@gmail.com>
2021-02-24 22:11:16 -0500

    some chagnes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 
-
-
 const router = Router();
+
 
 module.exports = (app) => {
     app.get("/testing", (req, res) => {
-        console.log(res);
         res.json({
           message: "hola frontend en react",
         });
--- src/server/config.js
+++ src/server/config.js
@@ -3,15 +3,17 @@ import express, { json } from "express";
 // import path from "path";
 import morgan from "morgan";
 import routes from '../routes'
-
+import cors from 'cors';
 // const routes = require("../routes/index");
 
 const config = (app) => {
 
   // Configuration
+  app.use(cors())
   app.set("port", process.env.PORT || 8000);
   app.use(json());
   app.use(morgan("dev"));
+
   // Static files
 
   // Routes from /routes index
