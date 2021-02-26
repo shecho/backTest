64c1f36347ad53a251884b83e9396390e7e36a75
shecho <shechovala@gmail.com>
2021-02-25 21:59:14 -0500

    car controller basic method

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -1,23 +1,25 @@
-import Car from '../models/Car'
+import Car from "../models/Car";
 
 const ctrl = {};
+ctrl.list = (req, res) => {
+  res.json({
+    message: "Listar todos los vehiculos",
+  });
+};
 ctrl.create = (req, res) => {
-        res.json({
-          message: "crear vehiculo",
-        });
- 
-}
+  res.json({
+    message: "crear vehiculo",
+  });
+};
 ctrl.update = (req, res) => {
-        res.json({
-          message: "Actualizar vehiculo",
-        });
- 
-}
+  res.json({
+    message: "Actualizar vehiculo",
+  });
+};
 ctrl.delete = (req, res) => {
-        res.json({
-          message: "eliminar vehiculo",
-        });
- 
-}
+  res.json({
+    message: "eliminar vehiculo",
+  });
+};
 
-export default ctrl
+export default ctrl;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import Home from '../controllers/homeController';
+import Car from '../controllers/carController';
 const router = Router();
 
 
 module.exports = (app) => {
     app.get("/", Home.wellcome);
 
-      app.use(router);
+    // Car Routes
+    app.get("/car",Car.list);
+    app.post("/car",Car.create);
+    app.put("/car",Car.update);
+    app.delete("/car",Car.delete);
+    // Schdeule Routes
+    app.use(router);
 }
