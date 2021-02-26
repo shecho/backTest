effa8440d438fa9a67478a32fde845abcbfb8ffe
shecho <shechovala@gmail.com>
2021-02-25 22:15:44 -0500

    some refactor and async server

---
+++ src/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const URI = process.env.MONGOOSE_URI
+  ? process.env.MONGOOSE_URI
+  : "mongodb://localhost/merndatabase";
+
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Database is connected");
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 import express from "express";
 import config from "./server/config";
-
-// const config = require("./server/config");
+import "./database";
 
 const app = config(express());
-app.listen(app.get(`port`), () => {
+
+async function main() {
+  await app.listen(app.get('port'));
   console.log(`Connected on port: http://localhost:${app.get("port")}/`);
-});
+}
+
+main();
