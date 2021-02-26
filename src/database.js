f23e77e8be983184aa8bc878dda8f1d7950e7a58
shecho <shechovala@gmail.com>
2021-02-25 22:32:25 -0500

    deprecated config

--- src/database.js
+++ src/database.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
-const URI = process.env.MONGOOSE_URI
-  ? process.env.MONGOOSE_URI
-  : "mongodb://localhost/merndatabase";
+const URI = process.env.MONGOOSE_URI || "mongodb://localhost/merndatabase";
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
-  useCreateIndex: true,
+  useCreateIndex: true, 
+  useUnifiedTopology: true,
 });
 const connection = mongoose.connection;
 
